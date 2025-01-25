43fedd98d1a8e33b4a0f2117a91f5f57c5ce03a1
Ali <aliwebdevf@gmail.com>
2025-01-25 20:10:15 +0330

    fixes-1

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,5 +1,5 @@
 const img = new Image();
-img.src = "../assets/img/login-bg.65cc0016.webp";
+img.src = "../assets/img/login-bg.d19ab698.webp";
 img.onload = () => {
   document.body.classList.add("bg-loaded");
 };
